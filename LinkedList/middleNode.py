7c730b827499ab6718cb00bdae8e86d0b9824f92
disharawat2507 <39236371+disharawat2507@users.noreply.github.com>
2026-01-11 17:33:07 -0800

    Create middleNode.py

---
+++ LinkedList/middleNode.py
@@ -0,0 +1,24 @@
+# Leetcode 876 Middle of the Linked List
+# Approach : If head is not there, the is no middle node, so return. Take 2 pointers, slow and fast. Slow will take one step and fast will take 2 steps. so when the fast reaches the end, slow will be in the middle.
+# Time Complexity : O(n)  
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next    
+
+        return slow    
