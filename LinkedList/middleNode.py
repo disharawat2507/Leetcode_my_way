444193b462ee2270d3fce312e25325e73e9451f1
disharawat2507 <39236371+disharawat2507@users.noreply.github.com>
2026-01-11 17:34:48 -0800

    Update middleNode.py

--- LinkedList/middleNode.py
+++ LinkedList/middleNode.py
@@ -1,7 +1,7 @@
 # Leetcode 876 Middle of the Linked List
 # Approach : If head is not there, the is no middle node, so return. Take 2 pointers, slow and fast. Slow will take one step and fast will take 2 steps. so when the fast reaches the end, slow will be in the middle.
 # Time Complexity : O(n)  
-
+# Space Complexity : O(1)
 
 # Definition for singly-linked list.
 # class ListNode:
