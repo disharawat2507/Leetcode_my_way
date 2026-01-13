4bf4cfa0d20c0802d76765c043111bc8be709e74
disharawat2507 <39236371+disharawat2507@users.noreply.github.com>
2026-01-13 12:01:46 -0800

    Update postordertraversal.py

--- Binary Tree/postordertraversal.py
+++ Binary Tree/postordertraversal.py
@@ -1,7 +1,7 @@
-Post Order Traversal.
-Approach : Postorder traversal in binary trees follows left, right , root. We always check if the value comming is not null. If true , we traverse first to left, then to right n then to root.
-Time Complexity : O(n)
-Space Complexity : O(h) where h is the height of the tree. In the worst case (a skewed tree), this is O(n); in a balanced tree, it is O(log n)
+# Post Order Traversal.
+# Approach : Postorder traversal in binary trees follows left, right , root. We always check if the value comming is not null. If true , we traverse first to left, then to right n then to root.
+# Time Complexity : O(n)
+# Space Complexity : O(h) where h is the height of the tree. In the worst case (a skewed tree), this is O(n); in a balanced tree, it is O(log n)
 
 class Solution:
     def postordertraversal(self, root):
