d61a17009aeb676d57aed270792f52d0a06c1dd1
disharawat2507 <39236371+disharawat2507@users.noreply.github.com>
2026-01-13 13:26:12 -0800

    Create maxhieght.py

---
+++ Binary Tree/maxhieght.py
@@ -0,0 +1,14 @@
+# Leetcode 104
+# Approach : always check if the root is None or not. If None, return 0. keep on calling method with root.left and root.right and then return 1 + max(left, right). 1 is added because it counts for the main head node.
+# Time Complexity : O(n)
+# Space Complexity : O(h) , h is the height of the tree
+
+class Solution:
+    def maxhieght(self, root):
+        if root is None:
+            return 0
+
+        left = self.maxheight(root.left)
+        right = self.maxheight(root.right)
+
+        return 1 +max(left, right)  
