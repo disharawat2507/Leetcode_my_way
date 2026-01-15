7e5931f9075c843d8e812d116a5588dbd9d9e39f
disharawat2507 <39236371+disharawat2507@users.noreply.github.com>
2026-01-15 10:53:16 -0800

    Create maxsum.py

---
+++ Binary Tree/maxsum.py
@@ -0,0 +1,19 @@
+Leetcode 124: Binary Tree Maximum Path Sum.
+Approach : here we are calculating left sum and right sum of all nodes including itself.And finding what is the maximum sum using maxlen varriable. Finally returning the maxlen.
+Time Complexity : O(n)
+Space Complexity : O(h) . H is the height of binary tree
+
+
+class Solution :
+    def mainsolution(self,root):
+        maxlen = [float('-inf')]
+        self.maxsum(maxlen,root)
+        return maxlen[0]
+
+    def maxsum(self, maxlen,root) :
+        if root is None:
+            return 0
+        left = max(0,self.maxsum(maxlen,root.left))
+        right = max(0,self.maxsum(maxlen,root.right))  
+        maxlen[0] = max(maxlen[0], left+right+root.val)  
+        return root.val + max(left,right)
